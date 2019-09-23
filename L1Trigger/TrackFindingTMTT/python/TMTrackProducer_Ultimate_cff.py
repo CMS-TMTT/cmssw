99f47323c7e0b4c4529105d33e4f0d6b6576a2ff
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2019-09-23 09:19:09 +0100

    Add Sept. 2019 MHT load balancing scheme

--- L1Trigger/TrackFindingTMTT/python/TMTrackProducer_Ultimate_cff.py
+++ L1Trigger/TrackFindingTMTT/python/TMTrackProducer_Ultimate_cff.py
@@ -55,5 +55,5 @@ TMTrackProducer.GenCuts.GenMinPt                    = cms.double(2.0)
 TMTrackProducer.HTArraySpecRphi.HoughMinPt          = cms.double(2.0)
 TMTrackProducer.HTArraySpecRphi.MiniHoughMinPt      = cms.double(3.0) # Mini-HT not used below this Pt, to reduce sensitivity to scattering.
 TMTrackProducer.L1TrackDef.MinStubLayers            = cms.uint32(4) 
-TMTrackProducer.HTFillingRphi.BusySectorMbinRanges = cms.vuint32(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 24) # Increased HT output bandwidth to cope with allowing 4 layers.
-TMTrackProducer.HTFillingRphi.BusySectorMbinOrder   = cms.vuint32(0,24, 2,26, 4,28, 6,30, 8,32, 10,34, 12,36, 14,38, 16,40, 18,42, 20,44, 22,46, 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47)
+TMTrackProducer.HTFillingRphi.BusySectorMbinRanges  = cms.vuint32(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 24)
+TMTrackProducer.HTFillingRphi.BusySectorMbinOrder   = cms.vuint32(0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47)
