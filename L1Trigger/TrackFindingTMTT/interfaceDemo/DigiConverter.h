46439d2f8fcf05ea1a8bd6d10d80f30fec2442b1
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2019-09-24 10:54:38 +0100

    Added phiSecInNonant to demo code

--- L1Trigger/TrackFindingTMTT/interfaceDemo/DigiConverter.h
+++ L1Trigger/TrackFindingTMTT/interfaceDemo/DigiConverter.h
@@ -24,8 +24,8 @@ class DigiConverter {
     ~DigiConverter() = default;
 
     DigiDTCStub makeDigiDTCStub( const TMTT::Stub& aDTCStub, uint32_t aDigiPhiSec ) const;
-    DigiHTStub makeDigiHTStub( const TMTT::Stub& aHTStub, uint32_t aSectorId, int cBin, int aChiZ, int aChiPhi, bool mSel) const;
-    DigiHTMiniStub makeDigiHTMiniStub( const TMTT::Stub& aHTMiniStub, uint32_t aSectorId, int8_t cBin, int8_t mBin) const;
+    DigiHTStub makeDigiHTStub( const TMTT::Stub& aHTStub, uint32_t aPhiSectorIdInNon, uint32_t aEtaSectorId, int cBin, int aChiZ, int aChiPhi, bool mSel) const;
+    DigiHTMiniStub makeDigiHTMiniStub( const TMTT::Stub& aHTMiniStub, uint32_t aPhiSectorIdInNon, uint32_t aEtaSectorId, int8_t cBin, int8_t mBin) const;
     DigiKF4Track makeDigiKF4Track( const TMTT::L1fittedTrack& aFitTrk ) const;
 
   private:
