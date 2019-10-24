392dbb10c2823821c18df89cb5419c83df5fd898
Emyr <emyr.clement@bristol.ac.uk>
2019-10-24 15:37:31 +0100

    A few fixes after rebasing to master.

--- L1Trigger/TrackFindingTMTT/interfaceDemo/DigiConverter.h
+++ L1Trigger/TrackFindingTMTT/interfaceDemo/DigiConverter.h
@@ -24,7 +24,7 @@ class DigiConverter {
     ~DigiConverter() = default;
 
     DigiDTCStub makeDigiDTCStub( const TMTT::Stub& aDTCStub, uint32_t aDigiPhiSec ) const;
-    DigiHTStub makeDigiHTStub( const TMTT::Stub& aHTStub, uint32_t aSectorId, int cBin, int aChiZ, int aChiPhi, bool mSel) const;
+    DigiHTStub makeDigiHTStub( const TMTT::Stub& aHTStub, uint32_t aPhiSectorIdInNon, uint32_t aEtaSectorId, int cBin, int aChiZ, int aChiPhi, bool mSel) const;
     DigiHTMiniStub makeDigiHTMiniStub( const TMTT::Stub& aHTMiniStub, const TMTT::L1track2D& aFitTrk, bool isFirstStubOnTrackCandidate, uint32_t aSectorId, int8_t cBin, int8_t mBin) const;
     DigiKF4Track makeDigiKF4Track( const TMTT::L1fittedTrack& aFitTrk ) const;
 
